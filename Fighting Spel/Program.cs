fe9c570379ad8c562e6bf87aa1b8f3df61cc6bb8
Albin <albin.nojback@gmail.com>
2020-11-03 18:08:43 +0100

    Beginning of Intro Screen

--- Fighting Spel/Program.cs
+++ Fighting Spel/Program.cs
@@ -7,12 +7,48 @@ namespace Fighting_Spel
     {
         static void Main(string[] args)
         {
-            Raylib.InitWindow(800, 600, "Fighting Spel");
+            //Define global game variables
+            string gameState = "intro";
+            const int windowX = 1200;
+            const int windowY = 800;
+
+
+            //Define Intro Screen Variables
+            //Font size * 2.6 = Font width
+            int playButtonRectX = windowX / 3;
+            int playButtonRectY = (windowY / 3) * 2;
+            int playButtonRectWidth = windowX / 3;
+            int playButtonRectHeight = windowY / 6;
+            float playButtonFontSizeF = (windowX / 3) / 2.6f - 3;
+            int playButtonFontSize = (int)playButtonFontSizeF;
+            float playButtonTextWidthF = playButtonFontSize * 2.6f;
+            int playButtonTextWidth = (int)playButtonTextWidthF;
+
+            Raylib.InitWindow(windowX, windowY, "Fighting Spel");
+
             while (!Raylib.WindowShouldClose())
             {
-                Raylib.BeginDrawing();
+                if (gameState == "intro")
+                {
+                    //Logik
+                    if (Raylib.GetMouseX() >= playButtonRectX && Raylib.GetMouseX() <= playButtonRectX + playButtonRectWidth && Raylib.GetMouseY() >= playButtonRectY && Raylib.GetMouseY() <= playButtonRectY + playButtonRectHeight && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                    {
 
-                Raylib.EndDrawing();
+                    }
+
+                    //Drawing
+                    Raylib.BeginDrawing();
+
+                    Raylib.ClearBackground(Color.BEIGE);
+                    Raylib.DrawRectangle(playButtonRectX, playButtonRectY, playButtonRectWidth, playButtonRectHeight, Color.BROWN);
+                    Raylib.DrawText("PLAY", playButtonRectX + (playButtonTextWidth / 2 - playButtonTextWidth / 2), playButtonRectY, playButtonFontSize, Color.WHITE);
+
+                    Raylib.EndDrawing();
+
+
+                }
+
+
             }
         }
     }
