f5d8e26098d08b2d8abc5051936e94f9cfdedede
Albin <albin.nojback@gmail.com>
2020-11-03 11:10:39 +0100

    Initial Commit

---
+++ Fighting Spel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Fighting_Spel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "Fighting Spel");
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
